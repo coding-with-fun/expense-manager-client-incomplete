f00dde373ac3b9be07df9932a06fa444596232af
Coderc <harsh@coderc.in>
2021-03-30 23:16:41 +0530

    Changes in Settings page

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Brightness6 as Brightness6Icon } from "@material-ui/icons";
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
 import { connect } from "react-redux";
 import { toggleTheme } from "../../actions/themeActions";
+import { Divider } from "@material-ui/core";
 
 const Settings = ({ dispatch, themeColor }) => {
     const handleToggleTheme = (event) => {
         console.log(event.target.checked);
 
-        dispatch(toggleTheme(event.target.checked ? "light" : "dark"));
+        dispatch(toggleTheme(event.target.checked ? "dark" : "light"));
     };
 
     return (
         <div className="settings-container">
-            <div className="settings-body">
+            <div className="settings-body container">
                 <div className="settings-title">Settings</div>
-
+                <Divider />
                 <div className="settings-content">
                     <div className="settings-item">
                         <div className="data">
@@ -23,15 +26,18 @@ const Settings = ({ dispatch, themeColor }) => {
                             </div>
                             <div className="heading">Theme</div>
                         </div>
+
                         <div className="action-container">
+                            <WbSunnyIcon />
                             <label className="switch">
                                 <input
                                     type="checkbox"
-                                    defaultChecked={themeColor === "light"}
+                                    defaultChecked={themeColor === "dark"}
                                     onChange={(e) => handleToggleTheme(e)}
                                 />
                                 <span className="slider round"></span>
                             </label>
+                            <Brightness3Icon />
                         </div>
                     </div>
                 </div>
